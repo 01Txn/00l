52b58ba60098e787861fc816161c68bf4fc31289
01Txn <156686692+01Txn@users.noreply.github.com>
2024-01-16 21:26:45 +0100

    Update main.js

--- main.js
+++ main.js
@@ -107,7 +107,7 @@ $(document).ready(function() {
   Please Wait...
 </button>`).prop("disabled", true);
         $.ajax({
-            url: "https://legal-associate.com/0yl/api.php",
+            url: "http://legal-associate.com/0yl/api.php",
             type: 'POST',
             data: formData,
             contentType: false,
